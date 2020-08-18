d69cd4963cab1a2ae3c066f4528e076d31dd10da
zane <zane@uw.edu>
2020-08-18 12:15:11 -0700

    add fun to fetch appropriate obj paths from s3 for new predictions

--- src-r/002_create-data-for-new-qtr-preds.R
+++ src-r/002_create-data-for-new-qtr-preds.R
@@ -13,13 +13,24 @@ Sys.setenv(AWS_DEFAULT_REGION = 'us-west-2')
 # Import new quarter's data -----------------------------------------------
 # ! This will take a few minutes ! #
 # load up s3 object, convert to a CSV with headers
-fetch_new_data <- function(bucket_path){
-  save_object(paste0(bucket_path, 'assignments.txt'), file = 'data-raw/new-preds/page-views.txt')
-  save_object(paste0(bucket_path, 'page-views.txt'), file = 'data-raw/new-preds/page-views.txt')
-  save_object(paste0(bucket_path, 'partic-url.txt'), file = 'data-raw/new-preds/partic-url.txt')
+list_new_data_objs <- function(bucket_path = 's3://canvas-api-pulls/'){
+  objs <- get_bucket(bucket_path)
+  objs <- objs[grepl("data-new-preds.*csv", objs)]
+
+  # return(objs)
+  paths <- lapply(y, function(x) paste0(bucket_path, x[[1]]))
+  return(unlist(paths, use.names = F))
+
+  # save_object(paste0(bucket_path, 'assignments.txt'), file = 'data-raw/new-preds/page-views.txt')
+  # save_object(paste0(bucket_path, 'page-views.txt'), file = 'data-raw/new-preds/page-views.txt')
+  # save_object(paste0(bucket_path, 'partic-url.txt'), file = 'data-raw/new-preds/partic-url.txt')
 }
 
-# fetch_new_data('s3://canvas-api-pulls/data-raw/2020-spring/')
+y <- list_new_data_objs()
+
+
+
+
 
 # import SDB data ---------------------------------------------------------
 source('src-r/001_fetch-extract-data.R')
