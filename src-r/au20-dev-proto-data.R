8861428985f97a4b454b34bf74c9ee1677b80f4c
zane <zane@uw.edu>
2020-09-18 14:01:39 -0700

    revise pull for observable/not-observable data in train/test

--- src-r/au20-dev-proto-data.R
+++ src-r/au20-dev-proto-data.R
@@ -12,8 +12,8 @@ data_path <- '../../Retention-Analytics-Dashboard/data-raw/spr20/'
 dirlist <- dirlist[-grep('03', dirlist)]
 wks <- seq_along(dirlist)
 
-prov_usr_path <- '~/Google Drive File Stream/My Drive/canvas-data/provisioning_csv_09_Sep_2020_1716420200909-18916-180jrta.csv'
-prov_crs_path <- '~/Google Drive File Stream/My Drive/canvas-data/provisioning_csv_14_Sep_2020_1719820200914-5303-al9flx.csv'
+PROV_USR_PATH <- '~/Google Drive File Stream/My Drive/canvas-data/provisioning_csv_09_Sep_2020_1716420200909-18916-180jrta.csv'
+PROV_CRS_PATH <- '~/Google Drive File Stream/My Drive/canvas-data/provisioning_csv_14_Sep_2020_1719820200914-5303-al9flx.csv'
 
 
 
@@ -41,9 +41,7 @@ fetch_cal_wks <- function(){
   return(res)
 }
 
-# build data from raw -----------------------------------------------------
-
-# tasks:
+# FUN to build assignment and participation data from raw api result files -----------------------------------------------------
 # list directories:
 #   for each directory:
        # combine the grades and assignments; adding the week #
@@ -110,7 +108,190 @@ mrg_partic <- function(dir, wk) {
 
 # Fetch funs for data from SDB ----------------------------------------
 
+# need these keys from EDW
+get_edw_keys <- function(){
+  con <- dbConnect(odbc(), 'sqlserver01')
+  skeys <- tbl(con, in_schema('sec', 'student_1')) %>%
+    select(system_key, uw_netid) %>%
+    collect() %>%
+    mutate_if(is.character, trimws) %>%
+    filter(uw_netid != "")
+
+  return(skeys)
+}
+
+
+# registration query ---------------------------------------------
+
+
+# TODO develop pull of reg data for end-yrq, transcript should ONLY be things we would observe for a quarter that isn't finished yet
+# In a hurry so cutting it to the actual quarter we need for this right now
+# TODO TECHNICAL DEBT
+
+fetch_reg_data <- function(){
+  con <- dbConnect(odbc(), 'sqlserver01')
+
+  reg <- tbl(con, in_schema('sec', 'registration')) %>%
+    filter(regis_yr == 2020,
+           regis_qtr == 4,  # 2 is for training, 4 is for new preds later
+           regis_class <= 4) %>%
+    mutate(yrq = regis_yr*10 + regis_qtr,
+           # eop = if_else(special_program %in% c(1, 2, 13, 14, 16, 17, 31, 32, 33), 1, 0),
+           regis_ncr) %>%
+    select(system_key,
+           yrq,
+           # resident,
+           # eop,
+           special_program,
+           regis_ncr,
+           regis_class,
+           # pc = pending_class,   # TODO - check on which to use - i think pmax of reg_class and transcript class, pending is T/F
+           tenth_day_credits,
+           current_credits)
+
+  regmaj <- tbl(con, in_schema('sec', 'registration_regis_col_major')) %>%
+    filter(regis_yr == 2020,
+           regis_qtr == 4,
+           index1 == 1) %>%
+    mutate(yrq = regis_yr*10 + regis_qtr) %>%
+    select(system_key,
+           yrq,
+           regis_major_abbr)
+
+  # some student 1 fields for most current data since many reg fields will be empty until ...?
+  # BUT keep regmaj if not blank
+  # PROBLEM: student_1 isn't helpful for training data b/c that would be peeking at future
+  s1_fields <- tbl(con, in_schema('sec', 'student_1')) %>%
+    filter(class <= 4,
+           projected_class <= 4) %>%
+    # mutate(eop = if_else(spcl_program %in% c(1, 2, 13, 14, 16, 17, 31, 32, 33), 1, 0)) %>%
+    select(system_key,
+           uw_netid,
+           projected_class,
+           spcl_program,
+           class,
+           resident,
+           num_holds,
+           ncr_code,
+           spp_qtrs_used,
+           tot_grade_points,
+           tot_graded_attmp,
+           tot_nongrd_earn,
+           tot_deductible,
+           tot_lowd_transfer,
+           tot_upd_transfer) %>%
+    left_join(
+      tbl(con, in_schema('sec', 'student_1_college_major')) %>%
+        filter(index1 == 1) %>%
+        select(system_key,
+               s1_maj = major_abbr))
+
+  reg_data <- reg %>%
+    left_join(regmaj) %>%
+    distinct() %>%
+    left_join(s1_fields) %>%
+    collect() %>%
+    mutate(eop = if_else(spcl_program %in% c(1, 2, 13, 14, 16, 17, 31, 32, 33) | special_program %in% c(1, 2, 13, 14, 16, 17, 31, 32, 33), 1, 0),
+           class = pmax(regis_class, class, projected_class, na.rm = T),
+           credits = pmax(tenth_day_credits, current_credits),
+           new_student = if_else(regis_ncr == 1 | ncr_code == 1, 1, 0),
+           major_abbr = if_else(is.na(regis_major_abbr), s1_maj, regis_major_abbr),
+           resident = if_else(resident %in% c(1, 2), 1, 0)) %>%
+    mutate_if(is.character, trimws) %>%
+    select(-spcl_program,
+           -special_program,
+           -regis_class,
+           -projected_class,
+           -tenth_day_credits,
+           -current_credits,
+           -regis_ncr,
+           -ncr_code,
+           -regis_major_abbr,
+           -s1_maj)
+
+  # I will count sections as an enrollment, label it to indicate that
+  # (don't take the max of index1, in this instance it indicates interactions with the system, not # of courses)
+  regcr <- tbl(con, in_schema('sec', 'registration_courses')) %>%
+    filter(regis_yr == 2020,
+           regis_qtr == 4,
+           request_status %in% c('A', 'C', 'R'),
+           credits > 0) %>%
+    mutate(yrq = regis_yr*10 + regis_qtr,
+           rep_course = if_else(`repeat` %in% c('1', '2' ,'3'), 1, 0),
+           # course = paste(crs_curric_abbr, crs_number, sep = "_"),
+           nonstd_grading = if_else(grading_system > 0, 1, 0)) %>%
+    select(system_key,
+           yrq,
+           nonstd_grading,
+           honor_course,
+           major_disallowed,
+           #course,
+           crs_curric_abbr,
+           crs_number,
+           crs_section_id,
+           rep_course,
+           crs_fee_amt) %>%
+    # join w/ major to calc course == major_abbr
+    left_join(regmaj) %>%
+    left_join( tbl(con, in_schema('sec', 'student_1_college_major')) %>%
+                 filter(index1 == 1) %>%
+                 select(system_key,
+                        major_abbr)
+    ) %>%
+    left_join( tbl(con, in_schema("EDWPresentation.sec", "dmSCH_dimCurriculumCourse")) %>%
+                 filter(FederalSTEMInd == "Y") %>%
+                 select(crs_curric_abbr = CurriculumCode,
+                        crs_number = CourseNbr) %>%
+                 distinct() %>%
+                 mutate(stem_course = 1)
+    ) %>%
+    collect() %>%
+    mutate_if(is.logical, as.numeric) %>%
+    mutate_if(is.character, trimws) %>%
+    mutate(# course = paste(crs_curric_abbr, crs_number, sep = "_"),
+      same_crs_maj = if_else(crs_curric_abbr == regis_major_abbr | crs_curric_abbr == major_abbr, 1, 0)) %>%
+    group_by(system_key, yrq) %>%
+    summarize(num_courses = n(),
+              n_nstd_grd = sum(nonstd_grading),
+              n_honors = sum(honor_course),
+              n_major_disallowed = sum(major_disallowed),
+              n_repeat = sum(rep_course),
+              tot_fees = sum(crs_fee_amt),
+              n_mjr_crs = sum(same_crs_maj, na.rm = T),
+              n_stem_crs = sum(stem_course, na.rm = T)) %>%
+    ungroup()
+
+  reg_data <- reg_data %>%
+    left_join(regcr) %>%
+    mutate_at(vars(resident:n_stem_crs, -major_abbr), replace_na, 0) %>%
+    mutate(ft = if_else(credits >= 12, 1, 0),
+           ft_creds_over = credits - 12,
+           premajor = if_else(grepl("PRE|EPRMJ", major_abbr) == T, 1, 0))
+    # replace_na(list(resident = 0,
+    #                 num_holds = 0,
+    #                 spp_qtrs_used = 0,
+    #                 n_mjr_crs = 0,
+    #                 n_stem_crs = 0,
+    #                 tot_fees = 0,
+    #                 n_courses = 0,
+    #                 n_nstd_grd = 0,
+    #                 n_honors = 0,
+    #                 n_major_disallowed = 0,
+    #                 n_repeat = 0,
+    #                 ))
+  return(reg_data)
+}
+reg_data <- fetch_reg_data()
+
+
+
+# transcript data ---------------------------------------------------------
+
+# transcripts are for training data and some elements for testing
+
 # return transcripts with majors
+# don't include information that we wouldn't be able to know at the _beginning_ of the time period for the training data
+# Some can be solved with lag for accumulated queries (and we will want that 20202 data for 20204)
 fetch_trans <- function(){
   con <- dbConnect(odbc(), 'sqlserver01')
 
@@ -134,16 +315,17 @@ fetch_trans <- function(){
            tenth_day_credits,
            num_courses,
            add_to_cum,
-           qtr_grade_points,
-           qtr_graded_attmp,
-           qtr_nongrd_earned,
-           qtr_deductible,
-           tran_branch,
-           tran_college,
-           tran_pathway,
-           tran_deg_level,
-           tran_deg_type,
-           tran_major_abbr)
+           qtr_grade_points,   # need to lag
+           qtr_graded_attmp,   # ...
+           qtr_nongrd_earned,  # ...
+           qtr_deductible,     # ...
+           # tran_branch,
+           # tran_college,
+           # tran_pathway,
+           # tran_deg_level,
+           # tran_deg_type,
+           tran_major_abbr) %>%
+    mutate(resident = if_else(resident %in% c(1, 2), 1, 0))
 
   tran_crs <- tbl(con, in_schema('sec', 'transcript_courses_taken')) %>%
     mutate(yrq = tran_yr * 10 + tran_qtr) %>%
@@ -157,25 +339,23 @@ fetch_trans <- function(){
            course_credits,
            course_branch,
            grade_system,    # 0 = standard; 5 = C/NC; 4 = S/NS; 9 = audit
-           grade,
+           grade,           # lag
            honor_course,
-           incomplete,
+           incomplete,      # lag
            repeat_course,
-           writing,
+           writing,         # lag - this doesn't show in timely fashion in registration data so we won't know
            major_disallowed) %>%
     distinct()
 
-  # TODO finish STEM indicator join
-
   # add STEM CIP indicator to courses
-  stem.courses <- tbl(con, in_schema("EDWPresentation.sec", "dmSCH_dimCurriculumCourse")) %>%
+  stem_courses <- tbl(con, in_schema("EDWPresentation.sec", "dmSCH_dimCurriculumCourse")) %>%
     filter(FederalSTEMInd == "Y") %>%
     select(dept_abbrev = CurriculumCode,
            course_number = CourseNbr) %>%
     distinct() %>%
     mutate(stem_course = 1)
 
-  res <- tran %>% left_join(tran_crs) %>% left_join(stem.courses) %>% collect()
+  res <- tran %>% left_join(tran_crs) %>% left_join(stem_courses) %>% collect()
 
   return(res)
 }
@@ -190,59 +370,50 @@ fetch_trans <- function(){
 a_all <- lapply(seq_along(dirlist), function(i) mrg_assgn(dirlist[[i]], wks[[i]]))
 # we could probably avoid the dirlist entirely with map() or mapply()
 p_all <- lapply(seq_along(dirlist), function(i) mrg_partic(dirlist[i], wks[i]))
-
 a_all <- bind_rows(a_all)
 p_all <- bind_rows(p_all)
 
-# Get/clean canvas provisioning -------------------------------------------
+# Import canvas provisioning -------------------------------------------
 
-# cur_usrs <- c(a_all$canvas_user_id, p_all$canvas_course_id) %>% unique()
-# cur_crss <- c(a_all$canvas_course_id, p_all$canvas_course_id) %>% unique()
-
-# read provisioning; these currently need periodic updates
-prov_usrs <- read_csv(prov_usr_path, col_types = 'nc--c------cc') %>% filter(status == 'active', created_by_sis == 'true') %>% select(-status, -created_by_sis)
-prov_crss <- read_csv(prov_crs_path, col_types = 'dc-cc--ncc----l') %>% filter(created_by_sis == T, status == 'active') %>% select(-status, -created_by_sis)
+# These need periodic updates
+prov_usrs <- read_csv(PROV_USR_PATH, col_types = 'nc--c------cc') %>% filter(status == 'active', created_by_sis == 'true') %>% select(-status, -created_by_sis)
+prov_crss <- read_csv(PROV_CRS_PATH, col_types = 'dc-cc--ncc----l') %>% filter(created_by_sis == T, status == 'active') %>% select(-status, -created_by_sis)
 
 # full join assignments and participation, leave NA's
-dat <- full_join(a_all, p_all)
-
 # combine provisioning with dat
-dat <- dat %>% inner_join(prov_crss) %>% inner_join(prov_usrs)
 
-# need keys from EDW
-get_edw_keys <- function(){
-  con <- dbConnect(odbc(), 'sqlserver01')
-  skeys <- tbl(con, in_schema('sec', 'student_1')) %>%
-    select(system_key, uw_netid) %>%
-    collect() %>%
-    mutate_if(is.character, trimws) %>%
-    filter(uw_netid != "")
+create_dat_from_canvas <- function(){
+  # read provisioning; these currently need periodic updates
 
-  return(skeys)
+  dat <- full_join(a_all, p_all)
+  dat <- dat %>% inner_join(prov_crss) %>% inner_join(prov_usrs)
+
+  # link student records:
+  # 1) edw <-> merged data on netid = login
+  dat <- dat %>% inner_join(get_edw_keys(), by = c('login_id' = 'uw_netid'))
+
+  # link courses:
+  # 1) turn dat/prov into dept + ### with yrq
+  xmat <- data.frame(str_split(dat$course_id, '-', simplify = T))
+  xmat$qtr <- match(xmat$X2, table = c('winter', 'spring', 'summer', 'autumn'))
+  # ymat <- data.frame(str_split(x$course_id, '-', simplify = T))
+  dat$dept_abbrev <- xmat$X3
+  dat$course <- paste(xmat$X3, xmat$X4, sep = "_")
+  dat$yrq <- as.numeric(paste0(xmat$X1, xmat$qtr))
+  dat$section <- xmat$X5
+
+  return(dat)
+
 }
-edw_keys <- get_edw_keys()
 
-# TODO finish matching w/ provisioning
-# link student records:
-# 1) edw <-> merged data on netid = login
-dat <- dat %>% inner_join(edw_keys, by = c('login_id' = 'uw_netid'))
+dat <- create_dat_from_canvas()
 
-# link courses:
-# 1) turn dat/prov into dept + ### with yrq
-xmat <- data.frame(str_split(dat$course_id, '-', simplify = T))
-xmat$qtr <- match(xmat$X2, table = c('winter', 'spring', 'summer', 'autumn'))
-# ymat <- data.frame(str_split(x$course_id, '-', simplify = T))
-dat$dept_abbrev <- xmat$X3
-dat$course <- paste(xmat$X3, xmat$X4, sep = "_")
-dat$yrq <- as.numeric(paste0(xmat$X1, xmat$qtr))
-dat$section <- xmat$X5
-
-
-rm(xmat, edw_keys)
-
-# Merging and feat engineering --------------------------------------------
+# Merging, FE w/ transcript data --------------------------------------------
 
 # transcript features (it's ok to have 20203+ so long as we're merging to the current file on yrq)
+# need to not include data that wouldn't be known, either by lagging before merging or by being careful about removing
+# vars from queries
+
 tranraw <- fetch_trans()
 
 ndfilt <- if_else(tranraw$tenth_day_credits == 0 &
@@ -296,14 +467,15 @@ tr1 <- tran %>%
   distinct() %>%
   group_by(system_key) %>%
   arrange(system_key, yrq) %>%
+  # TODO lag these as required for training data
   mutate(n_qtrs = row_number(yrq),
-         cum_premaj_qtrs = cumsum(premajor),
-         cum_courses = cumsum(num_courses),
-         cum_grdpts = cumsum(qtr_grade_points),
-         cum_attmp = cumsum(qtr_graded_attmp),
-         cum_nongrd = cumsum(qtr_nongrd_earned),
+         csum_premaj_qtrs = cumsum(premajor),
+         csum_courses = cumsum(num_courses),
+         csum_grdpts = cumsum(qtr_grade_points),
+         csum_attmp = cumsum(qtr_graded_attmp),
+         csum_nongrd = cumsum(qtr_nongrd_earned),
          cum_gpa = cum_grdpts / cum_attmp,
-         cum_honors = cumsum(honors_program),
+         csum_honors = cumsum(honors_program),
          mjr_change = if_else(tran_major_abbr == lag(tran_major_abbr), 0, 1),
          mjr_change = replace_na(mjr_change, 0),
          mjr_ch_count = cumsum(mjr_change)) %>%
@@ -384,13 +556,10 @@ tran <- tr1 %>%
 # NAN/NA aren't the same but this simplifies understanding later when trying to use the data b/c NAN values
 # create problems with funs or reading data into python which may not be obvious
 tran <- data.frame(lapply(tran, function(x) replace(x, is.nan(x), NA)))
-rm(tr1, trmany, stem, pmaj, maj)
+rm(tranraw, tr1, trmany, stem, pmaj, maj)
 
 # remember, we won't know cumulative information for the data we're trying to predict
 # - subset transcripts for final dataset
-
-# TODO decide how to deal with this - we won't see most of the 'current' data once registration starts
-# Probably should use sr_mini??
 
 # basically on day 1 we have resident, class, honors, and special program code (eop)
 # so let's assume we can use transcript to calculate _all_ of the above for whoever we need
@@ -405,76 +574,27 @@ rm(tr1, trmany, stem, pmaj, maj)
 # split on the vars we can keep that will go with the new q's data
 # TODO finish this
 # TODO write up the mapping so that the `tran` can combine with `reg`
-cur_vars <- tran %>%
+
+lagvars <- vars(qtr_gpa,
+                starts_with('cum_'),
+                mjr_change,
+                mjr_ch_count,
+                mean_grd,
+                starts_with('csum_'),
+                mean_stem_grd)
+# TODO mutate_at: lagvars
+tran_data <- tran %>%
   select(system_key,
          yrq,
          eop,
          resident,
          class,
          honors_program,
-         tenth_day_credits,
+         credits = tenth_day_credits,
          num_courses,
-         tran_branch,
-         premajor,
-         ft,
-         ft_creds_over,
          n_qtrs,
-         mjr_change,
-         mjr_ch_count)
-
-
-lagvars <- tran %>%
-  select(system_key,
-         yrq,
-         cum_premaj_qtrs,
-         cum_courses,
-         cum_grdpts,
-         cum_attmp,
-         cum_nongrd,
-         cum_gpa,
-         cum_honors,
          )
-
-
-
-
-# TODO DEV registration query ---------------------------------------------
-
-
-# TODO develop pull of reg data for end-yrq, transcript should ONLY be things we would observe for a quarter that isn't finished yet
-con <- dbConnect(odbc(), 'sqlserver01')
-
-reg <- tbl(con, in_schema('sec', 'registration')) %>%
-  filter(regis_yr >= 2015, regis_qtr >= 1,
-         regis_class <= 4) %>%
-  mutate(yrq = regis_yr*10 + regis_qtr,
-         eop = if_else(special_program %in% c(1, 2, 13, 14, 16, 17, 31, 32, 33), 1, 0)) %>%
-  select(system_key,
-         yrq,
-         resident,
-         eop,
-         class = regis_class,
-         pc = pending_class,   # TODO - check on which to use - i think pmax of reg_class and transcript class, pending is T/F
-         tenth_day_credits) %>%
-  # mutate()
-  collect()
-
-regmaj <- tbl(con, in_schema('sec', 'registration_regis_col_major')) %>%
-  mutate(yrq = regis_yr*10 + regis_qtr) %>%
-  select() %>%
-  collect()
-
-
-
-regcr <- tbl(con, in_schema('sec', 'registration_courses')) %>%
-  filter(regis_yr == 2020, regis_qtr == 4) %>%
-  collect()
-
-         # make available fields for model from regis data for a qtr w/ no transcripts
-
-
-
-
+  mutate_at(lagvars, lag)
 
 #
 # # Finalize merge + cleanup -----------------------------------------------------------------
